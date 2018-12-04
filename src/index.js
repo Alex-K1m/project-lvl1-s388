bd608365b05f82d6faf1d37900ffd23400a41081
Alex Kim <alex.k6@mail.ru>
2018-12-04 23:47:27 +0500

    add brain even game logic

--- src/index.js
+++ src/index.js
@@ -13,7 +13,38 @@ Welcome to the Brain Games!
 };
 
 const startBrainEven = () => {
-  console.log(`${userName}, the Brain even game has started.`);
+  // numbers for questions are generated randomly from [1; MAX_NUMBER]
+  const MAX_NUMBER = 100;
+  const NUM_OF_CORRECT_ANSWERS = 3; // to win the game
+  let number;
+  let isEven;
+  let answer;
+  let correctCounter = 0;
+
+  const generateNum = () => Math.ceil(Math.random() * MAX_NUMBER);
+  const ask = num => console.log(`Question: ${num}`);
+  const getAnswer = () => readlineSync.question('Your answer: ');
+
+  console.log('Answer "yes" if a number is even, otherwise answer "no".');
+  while (correctCounter < NUM_OF_CORRECT_ANSWERS) {
+    number = generateNum();
+    isEven = number % 2 === 0;
+    ask(number);
+    answer = getAnswer();
+    if ((isEven && answer === 'yes')
+    || (!isEven && answer === 'no')) {
+      console.log('Correct!');
+      correctCounter += 1;
+    } else {
+      console.log(
+        `"${answer}" is the wrong answer.`,
+        `The correct answer is "${isEven ? 'yes' : 'no'}".`,
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export { greeting, startBrainEven };
