fd234e7b7e51ae9030fd6550a89fe7bd2842c44b
Alex Kim <alex.k6@mail.ru>
2018-12-06 20:44:18 +0500

    fix generateProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,22 +7,23 @@ const MAX_STEP = 20;
 
 const task = 'What number is missing in the progression?';
 
-const generateProgression = () => {
-  const progression = [generateNum()];
-  const step = generateNum(MIN_STEP, MAX_STEP);
-  for (let i = 0; i < PROGRESSION_LENGTH - 1; i += 1) {
-    progression.push(progression[i] + step);
+const generateProgression = (firstElement, step) => {
+  const progression = [];
+  for (let i = 0; i < PROGRESSION_LENGTH; i += 1) {
+    progression.push(firstElement + step * i);
   }
   return progression;
 };
 
 const newRound = () => {
-  const progression = generateProgression();
+  const progression = generateProgression(
+    generateNum(),
+    generateNum(MIN_STEP, MAX_STEP),
+  );
   const randomIndex = generateNum(0, progression.length - 1);
   const correctAnswer = `${progression[randomIndex]}`;
   progression[randomIndex] = '..';
   const question = progression.join(' ');
-
   return { question, correctAnswer };
 };
 
