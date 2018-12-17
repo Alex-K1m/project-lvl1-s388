650001a60112d208498c5020c49752caddfc810a
Alex Kim <alex.k6@mail.ru>
2018-12-17 23:02:37 +0500

    simplified gcd algorithm

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,11 +4,8 @@ import startGame from '..';
 const task = 'Find the greatest common divisor of given numbers.';
 
 const findGcd = (num1, num2) => {
-  if (num1 !== 0 && num2 !== 0) {
-    if (num1 > num2) return findGcd(num1 % num2, num2);
-    return findGcd(num1, num2 % num1);
-  }
-  return num1 + num2;
+  if (num2 === 0) return num1;
+  return findGcd(num2, num1 % num2);
 };
 
 const newRound = () => {
